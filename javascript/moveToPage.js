357c5261e9423cd7575df393ccf0f30ed6293d02
san-sae <choi.yoo.lim.12@gmail.com>
2025-08-19 19:55:51 +0900

    [FEAT] 영화 페이지

---
+++ javascript/moveToPage.js
@@ -0,0 +1,9 @@
+homeBtn = document.querySelector(".nav__btns .home");
+homeBtn.addEventListener("click", () => {
+  window.location.href = "index.html"; 
+});
+
+movieBtn = document.querySelector(".nav__btns .movie");
+movieBtn.addEventListener("click", () => {
+  window.location.href = "movie.html"; 
+});
